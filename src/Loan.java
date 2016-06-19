e46f875991e6b33afc42e3a6e5020c060381a79a
Thomas Kennedy <thomaskennedy@Thomass-MacBook-Air.local>
2016-06-19 08:55:18 -0400

    Modifying to include monthly savings

--- src/Loan.java
+++ src/Loan.java
@@ -1,70 +1,77 @@
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-
 public class Loan {
 
 	private String name;
-	
-	private float interest;
-	
-	private float dealerDiscount;
-	
-	private float originationFees;
-	
+
+	private double interest;
+
+	private double dealerDiscount;
+
+	private double originationFees;
+
 	private int numberOfMonths;
 
-	private float principal;
-	
-	private Map<Integer, Float> reserveMap = new LinkedHashMap<Integer, Float>();
-	
-	public Loan(float interest, int numberOfMonths) {
+	private double principal;
+
+	private double downPayment;
+
+	private Map<Integer, Double> reserveMap = new LinkedHashMap<Integer, Double>();
+
+	public Loan(double interest, int numberOfMonths) {
 		this(interest, numberOfMonths, 0);
 	}
-	
-	public Loan(float interest, int numberOfMonths, int originationFees) {
-		this(interest, numberOfMonths, originationFees, 0);
+
+	public Loan(double interest, int numberOfMonths, double downPayment) {
+		this(interest, numberOfMonths, downPayment, 0);
 	}
 
-	public Loan(float interest, int numberOfMonths, int originationFees,
-			float dealerDiscount) {
+	public Loan(double interest, int numberOfMonths, double downPayment,
+			int originationFees) {
+		this(interest, numberOfMonths, downPayment, originationFees, 0);
+	}
+
+	public Loan(double interest, int numberOfMonths, double downPayment,
+			int originationFees, double dealerDiscount) {
 		this.interest = interest;
 		this.numberOfMonths = numberOfMonths;
 		this.originationFees = originationFees;
 		this.dealerDiscount = dealerDiscount;
-		if(numberOfMonths == 1) {
+		this.downPayment = downPayment;
+		if (numberOfMonths == 0) {
 			name = "CASH";
 		} else {
 			name = String.valueOf(interest)
 					+ "% "
 					+ String.valueOf(numberOfMonths)
-					+ " months "
+					+ " months " + (downPayment == 0 ? "" : String.format("$%,.2f",downPayment) + " Down")
 					+ (dealerDiscount == 0 ? "" : " Discount ("
 							+ dealerDiscount + ")");
 		}
 	}
 
-	public float getInterest() {
+	public double getInterest() {
 		return interest;
 	}
 
-	public void setInterest(float interest) {
+	public void setInterest(double interest) {
 		this.interest = interest;
 	}
 
-	public float getDealerDiscount() {
+	public double getDealerDiscount() {
 		return dealerDiscount;
 	}
 
-	public void setDealerDiscount(float dealerDiscount) {
+	public void setDealerDiscount(double dealerDiscount) {
 		this.dealerDiscount = dealerDiscount;
 	}
 
-	public float getOriginationFees() {
+	public double getOriginationFees() {
 		return originationFees;
 	}
 
-	public void setOriginationFees(float originationFees) {
+	public void setOriginationFees(double originationFees) {
 		this.originationFees = originationFees;
 	}
 
@@ -84,41 +91,54 @@ public class Loan {
 		this.name = name;
 	}
 
-	public void setPrincipal(float principal) {
+	public void setPrincipal(double principal) {
 		this.principal = principal;
 	}
 
 	public double getPayment() {
-		if(getRate() == 0) {
-			return getPrincipal()/getNumberOfMonths();
+		if (numberOfMonths == 0) {
+			return 0;
 		}
-		return getPrincipal()/((1-(1/Math.pow(1+getRate(),getNumberOfMonths())))/getRate()) ;
+		if (getRate() == 0) {
+			return getPrincipal() / getNumberOfMonths();
+		}
+		// return (getPrincipal() *
+		// getRate())/(1-Math.pow(1+getRate(),-getNumberOfMonths()));
+
+		return getPrincipal()
+				* (getRate() * Math.pow((1 + getRate()), getNumberOfMonths()))
+				/ (Math.pow((1 + getRate()), getNumberOfMonths()) - 1);
 	}
 
-	private float getRate() {
-		return getInterest()/1200;
+	private double getRate() {
+		return getInterest() / 1200;
 	}
 
-	private float getPrincipal() {
+	private double getPrincipal() {
 		// TODO Auto-generated method stub
 		return principal;
 	}
 
 	public void prettyPrintLoan() {
-		double totalpayment = getPayment() * getNumberOfMonths();
-		double totalInterest = totalpayment - getPrincipal();
-		System.out.format("\n%s",getName());
-		System.out.format("\n\tMonthly Payment: $%.2f", getPayment());
-		System.out.format("\n\tTotal Interest $%.2f",totalInterest);
-		System.out.format("\n\tTotal Amount: $%.2f",totalpayment);
+		double totalpayment = getPayment() * getNumberOfMonths()
+				+ getDownPayment();
+		double totalInterest = totalpayment - getPrincipal() - getDownPayment();
+		System.out.format("\n%s", getName());
+		System.out.format("\n\tMonthly Payment: $%,.2f", getPayment());
+		System.out.format("\n\tTotal Interest $%,.2f", totalInterest);
+		System.out.format("\n\tTotal Amount: $%,.2f", totalpayment);
 	}
 
-	public void addReserve(int i, float reserve) {
-		reserveMap.put(i,  reserve);
+	public void addReserve(int i, double reserve) {
+		reserveMap.put(i, reserve);
 	}
 
-	public Map<Integer, Float> getReserveMap() {
+	public Map<Integer, Double> getReserveMap() {
 		return reserveMap;
 	}
-	
+
+	public double getDownPayment() {
+		return downPayment;
+	}
+
 }
--- src/LoanDecisionMaker.java
+++ src/LoanDecisionMaker.java
@@ -1,52 +1,80 @@
-import java.util.Collections;
 import java.util.Map;
 
 
 public class LoanDecisionMaker {
 	
 	private static Loan[] loans = new Loan[] {
-	    new Loan(0,1),
-	    new Loan(0,36),
-	    new Loan(0.9f,60),
-	    new Loan(1.9f, 72),
-	    new Loan(2.99f, 60, 0, 750),
-	    new Loan(3.99f, 72, 0, 750)};
+//	    new Loan(0,0,23988),
+//	    new Loan(0.9d,63),
+	    new Loan(3d,144),
+	       new Loan(2.625d,120),
+	            new Loan(3.25d,240),
+	            new Loan(3.5d,360),
+	            new Loan(2.75d,180),
+
+//	    new Loan(3.375d,240),
+//	    new Loan(3.0d, 163),
+//	    new Loan(2.99d, 60, 0, 0, 750),
+//	    new Loan(3.99d, 72, 0, 0, 750
+//	        )
+	    };
 	
-	private static final float PRINCIPAL = 22988;
+	private static final int MONTHLY_SAVINGS = 1100;
+	private static final double PRINCIPAL = 115000;
 	
-	private static final float CAR_RESERVE = 36715.34f;
+	private static final double CAR_RESERVE = 0;
 	
 	public static void main(String[] args) {
 		int maxMonths = 0;
 		for(Loan loan : loans) {
-			loan.setPrincipal(PRINCIPAL - loan.getDealerDiscount() + loan.getOriginationFees());
+			loan.setPrincipal(PRINCIPAL - loan.getDealerDiscount() + loan.getOriginationFees() - loan.getDownPayment());
 			if(loan.getNumberOfMonths() > maxMonths) {
 				maxMonths = loan.getNumberOfMonths();
 			}
 			loan.prettyPrintLoan();
 		}
 		System.out.println("\n");
-		System.out.format("%40s", "");
+		
+		StringBuilder header = new StringBuilder();
 		for(int i=-5;i<15;i++) {
+			String bestOption ="";
+			double bestReserve = Double.MIN_VALUE;
 			for(Loan loan : loans) {
-				float reserve = CAR_RESERVE;
+				double reserve = CAR_RESERVE - loan.getDownPayment();
 				for(int j=0; j<maxMonths; j++) {
 					if(j<loan.getNumberOfMonths()) {
 						reserve -= loan.getPayment();
 					}
-					
-					float interest = (reserve* ((float) i/1200));
-					reserve += interest;
+					double interest = 0;
+					if(reserve > 0) {
+						interest = (reserve* ((double) i/1200));
+					}
+					reserve = reserve + interest + MONTHLY_SAVINGS;
+					if(reserve < 0) {
+//						loan.addReserve(i, reserve);
+						break;
+					}
+		
+				}
+				if(reserve > bestReserve) {
+					bestReserve= reserve;
+					bestOption = loan.getName();
 				}
 				loan.addReserve(i, reserve);
 			}
-			System.out.format("%s", "\t"+i+"%\t");
+			System.out.println(String.format("%15s", "Return Rate: " + i + "%") + " " + bestOption + " with a final reserve value of " + String.format("$%,.2f", bestReserve));
+			header.append("\t"+i+"%\t");
 		}
-		System.out.println("\n");
+		System.out.format("%40s", "");
+		System.out.println(header.toString() + "\n");
 		for(Loan loan : loans) {
 			System.out.format("%40s",loan.getName());
-			for(Map.Entry<Integer, Float> reserveEntry : loan.getReserveMap().entrySet()) {
-				System.out.format("\t$%.2f", reserveEntry.getValue());
+			for(Map.Entry<Integer, Double> reserveEntry : loan.getReserveMap().entrySet()) {
+				if(reserveEntry.getValue() < 0) {
+					System.err.format(String.format("\t$%,.2f", reserveEntry.getValue()));
+				} else {
+					System.out.format("\t$%,.2f", reserveEntry.getValue());
+				}
 			}
 			System.out.println("");
 		}
